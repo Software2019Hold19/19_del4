cfe7321a7d356e0e12855283fef358e7a61e1e8d
Thomas <thomas8@hohnen.dk>
2020-01-09 16:13:52 +0100

    Houses and Hotels now show on the board

--- src/main/java/GameBoard/GameBoard.java
+++ src/main/java/GameBoard/GameBoard.java
@@ -249,12 +249,12 @@ public class GameBoard {
 
 
         //Test
-        /*
+/*
         for (int i = 0; i < ownableBoard.length; i++) {
             ownableBoard[i].setOwner("Oli");
         }
-        */
 
+*/
     }
 
     public Field[] getBoard() {

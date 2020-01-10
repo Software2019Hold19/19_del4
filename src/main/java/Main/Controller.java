3e0f6f62b73b76bb82c767e65b219d4ea95fbc5f
Frederik Schrøder Koefoed <frederikkoefoeds@gmail.com>
2020-01-10 10:28:13 +0100

    Testing optimization

--- src/main/java/Main/Controller.java
+++ src/main/java/Main/Controller.java
@@ -86,6 +86,9 @@ public class Controller {
                 System.out.println("Turn Count total: " + turnCountTotal);
             }
         }
+        if (testing) {
+            setTesting();
+        }
         int max = pLst[0].getBal();
         String winner = pLst[0].getName();
         for (Player p : pLst){
@@ -94,6 +97,7 @@ public class Controller {
                 max = p.getBal();
             }
         }
+        System.out.println(turnCountTotal);
         gui.showMessage(String.format(lib.text.get("Winner"), winner));
     }
 
@@ -266,9 +270,9 @@ public class Controller {
     }
 
     public void setTesting() {
-        testing = true;
+        testing = !testing;
         deck = new ChanceDeck(lib, testing);
-        gui.setTesting();
+        gui.setTesting(testing);
     }
     
 }
